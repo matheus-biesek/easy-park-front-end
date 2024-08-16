7c6c60514ccacd895ad86172a999048346363ad7
matheus-biesek <md.biesek@gmail.com>
2024-08-16 01:50:08 -0300

    Remoção do bloqueio após o usuario clicar no botão

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -3,20 +3,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from '../../service/login/login.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']  // Corrigido de 'styleUrl' para 'styleUrls'
 })
 
-
 export class LoginComponent {
+
   loginForm: FormGroup;
-  isSubmitting = false;
   errorMessage: string | null = null;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private router: Router,
+    ) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
       password: ['', [Validators.required]]
@@ -24,16 +26,17 @@ export class LoginComponent {
   }
 
   onSubmit(): void {
+
     if (this.loginForm.invalid) {
       return;
     }
 
-    this.isSubmitting = true;
     this.errorMessage = null;
 
     this.loginService.login(this.loginForm.value).subscribe({
       next: (response) => {
         if (response && response.token) {
+          localStorage.removeItem('authToken');
           localStorage.setItem('authToken', response.token);
           this.router.navigate(['/status-vacancies']); 
         } else {
@@ -42,9 +45,6 @@ export class LoginComponent {
       },
       error: (error) => {
         this.errorMessage = 'Login falhou. Verifique suas credenciais.';
-      },
-      complete: () => {
-        this.isSubmitting = false;
       }
     });
   }
