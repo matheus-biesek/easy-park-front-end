ff8dd5cd3e5f6237852de97018eb95001d639976
matheus-biesek <md.biesek@gmail.com>
2024-08-16 13:32:09 -0300

    alterando url da requisição para atualizar role de usuário

--- src/app/service/update-role/update-role.service.ts
+++ src/app/service/update-role/update-role.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UpdateRoleService {
-  private apiUrl = 'http://localhost:8080/user/update-role';
+  private apiUrl = 'http://localhost:8080/auth/update-role';
 
   constructor(
     private http: HttpClient,
