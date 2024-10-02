ac7281b33fb444daba7e59008a15dc82c13e2ed6
matheus dominguez biesek <md.biesek@gmail.com>
2024-10-02 03:20:23 -0300

    ajuste para atualização automatica do status das vagas

--- src/app/component/vacancy/status-vacancies/status-vacancies.component.ts
+++ src/app/component/vacancy/status-vacancies/status-vacancies.component.ts
@@ -1,28 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Vacancy } from '../../../interface/vacancy.model';
 import { VacancyService } from '../../../service/vacancy.service';
+import { interval, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-status-vacancies',
   templateUrl: './status-vacancies.component.html',
   styleUrls: ['./status-vacancies.component.css']
 })
-export class StatusVacanciesComponent implements OnInit {
+export class StatusVacanciesComponent implements OnInit, OnDestroy {
 
   vacancies: Vacancy[] = [];
+  subscription: Subscription | undefined;
 
   constructor(private vacancyService: VacancyService) {}
 
   ngOnInit(): void {
-    this.vacancyService.statusVacancies().subscribe({
+    // Chamada inicial para carregar o status das vagas imediatamente
+    this.loadVacanciesStatus();
+
+    // Inicia um loop a cada 3 segundos (3000 ms)
+    this.subscription = interval(3000).pipe(
+      // Faz a requisição a cada 3 segundos
+      switchMap(() => this.vacancyService.statusVacancies())
+    ).subscribe({
       next: (response: Vacancy[]) => {
-        // Ordena as vagas pela posição antes de atribuí-las à variável vacancies
         this.vacancies = response.sort((a, b) => a.position - b.position);
       },
       error: (err) => {
         console.error('Erro ao carregar status das vagas', err);
       }
     });
+  }
+
+  // Método para carregar o status das vagas
+  private loadVacanciesStatus(): void {
+    this.vacancyService.statusVacancies().subscribe({
+      next: (response: Vacancy[]) => {
+        this.vacancies = response.sort((a, b) => a.position - b.position);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar status das vagas', err);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Cancela a assinatura quando o componente for destruído para evitar vazamentos de memória
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   // Retorna a classe CSS com base no status
