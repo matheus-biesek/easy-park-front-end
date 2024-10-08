0fd80151d1845f756b7499fe590621849b5ad3fc
matheus dominguez biesek <md.biesek@gmail.com>
2024-10-07 22:58:42 -0300

    Colocando temporizador na requisição que coleta a mensagem do adm

--- src/app/component/adm/status-adm-alert/status-adm-alert.component.ts
+++ src/app/component/adm/status-adm-alert/status-adm-alert.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ParkingLotService } from '../../../service/parking-lot.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-status-adm-alert',
   templateUrl: './status-adm-alert.component.html',
-  styleUrl: './status-adm-alert.component.css'
+  styleUrls: ['./status-adm-alert.component.css'] // Corrigido 'styleUrls' no plural
 })
-export class StatusAdmAlertComponent {
+export class StatusAdmAlertComponent implements OnInit, OnDestroy {
   admAlert: string = '';
   errorMessage: string = '';
+  subscription: Subscription | undefined;
 
-  constructor(
-    private parkingLotService: ParkingLotService
-  ) {}
+  constructor(private parkingLotService: ParkingLotService) {}
 
   ngOnInit(): void {
+    // Chamada inicial para carregar a mensagem do administrador
     this.loadAdmAlert();
+
+    // Iniciar o loop de requisição a cada 6 segundos (6000 ms)
+    this.subscription = interval(6000).subscribe(() => {
+      this.loadAdmAlert();
+    });
   }
 
+  // Método para carregar a mensagem do administrador
   loadAdmAlert(): void {
     this.parkingLotService.collectAdmAlert().subscribe({
       next: (response: string) => {
@@ -27,5 +34,12 @@ export class StatusAdmAlertComponent {
         this.errorMessage = 'Erro ao carregar a mensagem do administrador';
       }
     });
+  }
+
+  // Desinscrever do intervalo para evitar vazamentos de memória
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
